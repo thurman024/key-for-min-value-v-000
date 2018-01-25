90ff7815ae504acee75b0701841ae66fa57c81ba
Stephen Thurman <thurman024@gmail.com>
2018-01-25 21:19:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
     return nil
   end
   lowest_value = nil
+  key_for_min_value = nil
   name_hash.each do |key, value|
     # binding.pry
     if lowest_value == nil || lowest_value > value
